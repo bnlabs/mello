8621303734763c42badc3106d52670be9944ea86
Hy0tic <vothanhdat278@gmail.com>
2024-03-28 07:31:23 -0400

    change var names for input divs

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,21 @@
+
 <template>
-    <div class="flex h-screen items-center justify-center bg-green-500">
-        <div class="flex flex-col w-[650px] h-[500px] items-center justify-center shadow-2xl rounded-2xl bg-black">
+
+    <div class="flex h-screen items-center justify-center bg-green-600">
+        <div class="flex flex-col w-[650px] h-[500px] gap-4 items-center justify-center shadow-2xl rounded-2xl bg-black">
     
+            <!-- Username and LobbyId fields -->
             <div class="flex flex-col gap-2 text-white">
                 <label for="username">Username</label>
                 <InputText id="username" v-model="value" aria-describedby="username-help" />
-                <label for="username">LobbyId</label>
-                <InputText id="username" v-model="value" aria-describedby="username-help" />
+                <label for="lobbyId">LobbyId</label>
+                <InputText id="lobbyId" v-model="value" aria-describedby="lobbyId-help" />
             </div>
+
+
+            <input type="submit" value="Join Lobby" class="bg-green-600 text-white rounded-md h-12 w-32">
         </div>
+
     </div>
 </template>
 
