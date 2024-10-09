b71aa60d0545a03de4e5ffde8735b90c47c3e056
Hy0tic <vothanhdat278@gmail.com>
2024-10-09 01:12:04 -0500

    add param for publish and subscribing permission

--- server/api/getLiveKitToken.get.ts
+++ server/api/getLiveKitToken.get.ts
@@ -5,6 +5,8 @@ export default defineEventHandler(async (event: any) => {
 	const query = getQuery(event)
 	const room:string = query.room?.toString() ?? ""
 	const username = query.username?.toString() ?? ""
+	const canPublish: boolean = query.canPublish === "true"
+	const canSubscribe: boolean = query.canSubscribe === "true"
 
 	if(!room || !username) {
 		return {
@@ -14,12 +16,12 @@ export default defineEventHandler(async (event: any) => {
 	}
 
 	return {
-		token: await createToken(room, username)
+		token: await createToken(room, username, canPublish, canSubscribe)
 	}
 })
 
 
-const createToken = async (room: string, username: string) => {
+const createToken = async (room: string, username: string, canPublish: boolean, canSubscribe: boolean) => {
 	const at = new AccessToken(
 		process.env.LIVEKIT_API_KEY,
 		process.env.LIVEKIT_API_SECRET,
@@ -29,7 +31,7 @@ const createToken = async (room: string, username: string) => {
 			ttl: "10m",
 		},
 	)
-	at.addGrant({ roomJoin: true, room: room })
+	at.addGrant({ roomJoin: true, room: room , canPublish: canPublish, canSubscribe: canSubscribe })
 	
 
 	const token = await at.toJwt()
