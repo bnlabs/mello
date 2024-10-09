06cfbb156cabb2a758a662906c3687261a54c5d3
Hy0tic <vothanhdat278@gmail.com>
2024-10-08 20:48:01 -0500

    discard event

--- server/api/health.ts
+++ server/api/health.ts
@@ -1,5 +1,5 @@
 
-export default defineEventHandler(async (event) => {
+export default defineEventHandler(async (_) => {
     const options:any = { 
         year: 'numeric', 
         month: 'long', 
