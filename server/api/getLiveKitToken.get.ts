7b09e3793574e62d346a5bf09e9e5a1015c92aaa
Hy0tic <vothanhdat278@gmail.com>
2024-10-09 18:36:28 -0500

    getToken endpoint return host of the lobby

--- server/api/getLiveKitToken.get.ts
+++ server/api/getLiveKitToken.get.ts
@@ -1,4 +1,5 @@
-import { AccessToken } from "livekit-server-sdk"
+import { AccessToken, ParticipantInfo, } from "livekit-server-sdk"
+import { livekitApiKey, livekitApiSecret, roomService } from "../utils/livekit"
 
 export default defineEventHandler(async (event) => {
 	const query = getQuery(event)
@@ -10,12 +11,32 @@ export default defineEventHandler(async (event) => {
 	if (!room || !username) {
 		return {
 			statusCode: 400,
-			message: "must provide room and username",
+			message: "Must provide room and username",
 		}
 	}
 
+	let res: ParticipantInfo[]
+	let publishers: ParticipantInfo[] = []
+	// check if host already exist:
+	try {
+		res = await roomService.listParticipants(room);
+		publishers = res.filter(participant => participant.permission?.canPublish)
+
+		if(canPublish && publishers.length > 0){
+			return {
+				statusCode: 400,
+				message: "There is already a host for this lobby"
+			}
+		}
+	} catch {
+		// room doesnt exist so we can just continue to generate and return token
+	}
+	
+
 	return {
+		statusCode: 200,
 		token: await createToken(room, username, canPublish, canSubscribe),
+		host: publishers[0].name
 	}
 })
 
@@ -26,8 +47,8 @@ const createToken = async (
 	canSubscribe: boolean,
 ) => {
 	const at = new AccessToken(
-		process.env.LIVEKIT_API_KEY,
-		process.env.LIVEKIT_API_SECRET,
+		livekitApiKey,
+		livekitApiSecret,
 		{
 			identity: username,
 			// Token to expire after 10 minutes,
