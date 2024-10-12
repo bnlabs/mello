b72b0f2bfda23f5a4254630a8950de61704989ab
Hy0tic <vothanhdat278@gmail.com>
2024-10-12 06:20:16 -0500

    add alt tag to image

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -3,13 +3,13 @@
 		class="flex h-20 w-full flex-row items-center justify-between bg-[rgb(31,41,55)]"
 	>
 		<div class="m-10 flex flex-row items-center gap-2 text-3xl">
-			<img src="../public/bnlab.png" class="h-12" />
+			<img src="../public/bnlab.png" class="h-12" alt="bnlab" />
 			<div class="font-mono font-bold text-[rgb(200,200,200)]">mello</div>
 		</div>
 
 		<div class="hover:filter: brightness(2) m-10 flex flex-row">
 			<a href="https://github.com/bnlabs/mello">
-				<img src="../public/github-mark-white.svg" class="h-8" />
+				<img src="../public/github-mark-white.svg" class="h-8" alt="github" />
 			</a>
 		</div>
 	</div>
