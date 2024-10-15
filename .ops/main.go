792ede662126d4aca1ab2a843e570b6e89b7a181
Hy0tic <vothanhdat278@gmail.com>
2024-10-15 18:39:42 -0500

    move build command to its own file

--- .ops/main.go
+++ .ops/main.go
@@ -1,9 +1,6 @@
 package main
 
 import (
-	"log"
-
-	"lesiw.io/cmdio/sys"
 	"lesiw.io/ops"
 )
 
@@ -15,31 +12,4 @@ func main() {
 
 func (Ops) Hello() {
 	println("Hello world!")
-}
-
-func (Ops) Build () {
-	var rnr = sys.Runner().WithEnv(map[string]string{
-		"PKGNAME": "cmdio",
-	})
-	defer rnr.Close()
-
-	err := rnr.Run("echo", "hello from", rnr.Env("PKGNAME"))
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	err = rnr.Run(useNpmOrPnpm(rnr), "install")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	err = rnr.Run(useNpmOrPnpm(rnr), "run", "build")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	err = rnr.Run("echo", "goodbye from", rnr.Env("PKGNAME"))
-	if err != nil {
-		log.Fatal(err)
-	}
 }
