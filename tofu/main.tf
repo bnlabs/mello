f78a9c8653fdb9d378793035b268f932d47d449e
Hy0tic <vothanhdat278@gmail.com>
2024-10-19 07:14:50 -0500

    remove old code

--- tofu/main.tf
+++ tofu/main.tf
@@ -1,27 +1,3 @@
-# terraform {
-#   required_providers {
-#     aws = {
-#         source = "hashicorp/aws",
-#         version =  "~> 5.31"
-#     }
-#   }
-
-#   required_version = ">= 1.2.0"
-
-#   backend "s3" {
-#     bucket         = "mello-state-bucket"
-#     key            = "state"
-#     region         = "us-east-1"
-#     dynamodb_table = "mello-tofu-state-lock"
-#     encrypt        = false
-#   }
-# }
-
-# provider "aws" {
-#     region  = "us-east-1"
-#     # profile = "default" # uncomment for local development
-# }
-
 terraform {
   required_providers {
     vercel = {
