a998078a2a54cc8066773c36955ef68d973decde
Hy0tic <vothanhdat278@gmail.com>
2024-10-19 07:01:13 -0500

    remove step for configure aws credentials, add vercel token

--- tofu/main.tf
+++ tofu/main.tf
@@ -34,5 +34,11 @@ terraform {
 provider "vercel" {
   # Or omit this for the api_token to be read
   # from the VERCEL_API_TOKEN environment variable
-  api_token = ""
+  api_token = var.vercel_api_token
+}
+
+variable "vercel_api_token" {
+  description = "API token for Vercel"
+  type        = string
+  default     = ""
 }
