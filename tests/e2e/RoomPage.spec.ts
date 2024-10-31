8771df969d517b97331bdde1fcd9ba91aa12f236
Hy0tic <vothanhdat278@gmail.com>
2024-10-31 16:01:16 -0500

    add message test

--- tests/e2e/RoomPage.spec.ts
+++ tests/e2e/RoomPage.spec.ts
@@ -1,10 +1,11 @@
-import { test } from "@playwright/test"
+import { test, expect } from "@playwright/test"
 import { config } from "./E2eConfig"
 import {
 	AssertRoomHosted,
 	AssertRoomJoined,
 	QuickHostRoom
 } from "./HelperFunction"
+
 
 test("User can join room", async ({ page, browser }) => {
 	const { usernameInput, roomInput } = await QuickHostRoom(page)
@@ -22,4 +23,35 @@ test("User can join room", async ({ page, browser }) => {
 
 	// assert join successful
 	await AssertRoomJoined(page2, user2name, roomInput)
+})
+
+test("User can send chat message in lobby", async ({ page, browser }) => {
+	const { usernameInput, roomInput } = await QuickHostRoom(page)
+
+	await AssertRoomHosted(page, usernameInput, roomInput)
+	await page.waitForTimeout(6000)
+
+	// User 2
+	const page2 = await browser.newPage()
+	const user2name = "user2"
+
+	await page2.goto(
+		`${config.baseURL}room?username=${user2name}&room=${roomInput}&isHost=false&serverSideStreaming=false`
+	)
+
+	// assert join successful
+	await AssertRoomJoined(page2, user2name, roomInput)
+
+	const messageEntry = page2.locator("#chat-message-input")
+
+	// send chat message
+	await messageEntry.fill("Hello world!")
+	await page2.keyboard.press('Enter')
+
+	await page2.waitForTimeout(5000)
+
+	// user 1 can see message from user 2
+	const textLocator = page.locator('text="Hello world!"')
+	await expect(textLocator).toBeVisible()
+
 })
